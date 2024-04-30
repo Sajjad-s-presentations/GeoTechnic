a5c3dae8b1fdaa7ea822b2d15f24708f9b38a0ec
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-04-30 19:03:05 +0330

    SQLALCHEMY_TRACK_MODIFICATIONS

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
