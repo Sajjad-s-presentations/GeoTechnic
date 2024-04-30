c9d643424696d8886f7dd72290897186411b6e29
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-04-30 18:32:39 +0330

    libs

--- app.py
+++ app.py
@@ -4,11 +4,3 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
