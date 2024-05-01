6e0498c9385c503b9b373e6e27e4d120cf47e36b
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-01 18:40:14 +0330

    create_app

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder="templates")
+    app.config['SQL_DATABASE_URI'] = 'sqlite:///./testdb.db'
+
+    db.init_app(app)
+
+    migrate = Migrate(app, db)
+
+
+    return app
