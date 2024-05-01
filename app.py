51756653f079ceea4b9fb586656c82394562b3e7
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-01 15:57:16 +0330

    define model

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 # initialize the app with the extension
 db.init_app(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String)
