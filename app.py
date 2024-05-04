0d2cb8dd9a7dd11a8374fbd659063635382a1e87
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-04 18:30:01 +0330

    connect to my sql

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flask'
+app.config['MYSQL_DB'] = 'test'
 
 mysql = MySQL(app)
