21b69faa0da4c93f45916370fb528007076a6d41
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-05-07 11:40:47 +0330

    administrator login libs

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
+
 from models import DB
 app = Flask(__name__)
 
